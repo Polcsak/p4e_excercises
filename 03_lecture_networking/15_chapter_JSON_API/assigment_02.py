8232c5a51addd1d70d04d254ea96c86865505e82
Jakub Polcsak <jakub.polcsak@gmail.com>
2023-02-12 18:23:16 +0100

    Finished 3rd course

---
+++ 03_lecture_networking/15_chapter_JSON_API/assigment_02.py
@@ -0,0 +1,61 @@
+# """Calling a JSON API
+
+# In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geojson.py. The program will prompt for a location, contact a web service and retrieve JSON for the web service and parse that data, and retrieve the first place_id from the JSON. A place ID is a textual identifier that uniquely identifies a place as within Google Maps.
+# API End Points
+
+# To complete this assignment, you should use this API endpoint that has a static subset of the Google Data:
+
+# http://py4e-data.dr-chuck.net/geojson?
+# This API uses the same parameter (address) as the Google API. This API also has no rate limit so you can test as often as you like. If you visit the URL with no parameters, you get a list of all of the address values which can be used with this API.
+# To call the API, you need to provide address that you are requesting as the address= parameter that is properly URL encoded using the urllib.urlencode() fuction as shown in http://www.py4e.com/code3/geojson.py"""
+
+
+# Enter location: University of Twente
+# Retrieving  http://python-data.dr-chuck.net/geojson?#sensor=false&address=University+of+Twente
+# Retrieved 2124 characters
+# Place id ChIJPZ9qp0tvv4cRb5oLVI9wra8
+
+
+# Python talking to google API
+
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else:
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1:
+        break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False:
+        parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    #print('Retrieved', len(data), 'characters')
+    json_obj = json.loads(data)
+
+    place_id = json_obj["results"][0]["place_id"]
+    #print("Place id", place_id)
